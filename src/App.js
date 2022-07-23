dce9fd602b7772c81250a9e952e81aa7e19e421a
Agustin <pierdoagus2@gmail.com>
2022-07-23 04:40:26 -0300

    Entrega Navbar

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  // const imagen = <img src="url" alt="imagen" />
+
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>
+        <p>Lista de Productos</p>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
